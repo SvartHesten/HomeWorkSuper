c361dfea2d30ac300a473cc2a2989a56eb11df13
Alexander Terminassian <suurikarhu54@yahoo.com>
2020-09-05 19:09:57 -0700

    First file

---
+++ FileOne.py
@@ -0,0 +1,18 @@
+# input a number, test if it is odd or even
+
+name = input(" What is Your name, my friend? ")
+while True:
+    try:
+        test_number = int(input(f" Dear {name}, please enter the number to test"))
+    except ValueError:
+        print("Ooops, not a valid entry......")
+        continue
+    if test_number % 2 == 0:
+        print(f" {name}, the given  number {test_number} is even ")
+        break
+    elif test_number % 2 != 0:
+        print(f" {name}, the given  number {test_number} is odd")
+        break
+print(" Have a great day!")
+
+
