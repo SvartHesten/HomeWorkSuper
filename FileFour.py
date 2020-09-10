7ee49911902df2151ba9f814815f9c96b966ef33
Alexander <69052018+SvartHesten@users.noreply.github.com>
2020-09-10 15:30:13 -0700

    Update FileFour.py

--- FileFour.py
+++ FileFour.py
@@ -3,11 +3,11 @@
 
 number_count = 0
 while number_count < 1:
-    number_string = input("Enter the positive integer value")
+    number_string = input("Enter the positive integer value please")
     try:
         number = int(number_string)
     except ValueError:
-        print(" O o ops, not a valid number")
+        print(" O ooo ops, not a valid number")
         continue
     number_count += 1
 if number > 0:
